a9e648c9eb258eb7d4d4c3dd68b8fda806693ec7
Max Mechanic <maxmechanic@users.noreply.github.com>
2015-10-19 10:58:54 -0400

    Add higher-order reducer for watching specific actions

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 import assert from 'assert'
-import ignoreActions from '../src/index'
+import {ignoreActions, includeActions} from '../src/index'
 
 describe('ignoreActions()', () => {
   let reducer = (state, action) => {
@@ -47,5 +47,54 @@ describe('ignoreActions()', () => {
     assert.equal(
       ignoringReducer('testing', action),
       'testing')
+  })
+})
+
+describe('includeActions()', () => {
+  let reducer = (state, action) => {
+    switch (action.type) {
+      case 'FOO':
+        return 'foo-state'
+      case 'BAR':
+        return 'bar-state'
+      default:
+        return 'default-state'
+    }
+  }
+
+  it('should include actions with specified types in array', () => {
+    let includingReducer = includeActions(reducer, ['BAR'])
+    let action = { type: 'BAR' }
+
+    assert.equal(
+      includingReducer('testing', action),
+      'bar-state')
+  })
+
+  it('should exclude actions that do not have types in array', () => {
+    let includingReducer = includeActions(reducer, ['BAR'])
+    let action = { type: 'FOO' }
+
+    assert.equal(
+      includingReducer('testing', action),
+      'testing')
+  })
+
+  it('should exclude all actions when no action types array is specified', () => {
+    let includingReducer = includeActions(reducer)
+    let action = { type: 'BAZ' }
+
+    assert.equal(
+      includingReducer('testing', action),
+      'testing')
+  })
+
+  it('should work with a predicate function for actions', () => {
+    let includingReducer = includeActions(reducer, (a) => a.valid)
+    let action = { type: 'BAR', valid: true }
+
+    assert.equal(
+      includingReducer('testing', action),
+      'bar-state')
   })
 })
