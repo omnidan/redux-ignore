bc0781a02d796f0e9e241e0c7f8ea86cff3a5055
Daniel Bugl <me@omnidan.net>
2016-02-29 11:26:06 +0100

    upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import isFunction from 'lodash/lang/isFunction'
+function isFunction (obj) {
+  return !!(obj && obj.constructor && obj.call && obj.apply)
+}
 
 function createActionHandler (ignore) {
   // redux-ignore higher order reducer
